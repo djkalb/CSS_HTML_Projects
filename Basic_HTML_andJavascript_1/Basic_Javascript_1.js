9f11af875ce7d50c2d2c32349527152ee0ec1307
djkalb <71739290+djkalb@users.noreply.github.com>
2022-01-19 16:36:36 -0800

    submission for basic html assignment

---
+++ Basic_HTML_andJavascript_1/Basic_Javascript_1.js
@@ -0,0 +1,6 @@
+function My_First_Funtion() {
+				var String = "kiss me, Im Irish";
+				var result = String.fontcolor("green");
+				document.getElementById("Irish").innerHTML =
+				result;
+				}
